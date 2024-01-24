01f51bc86eef1f741bd6b714d2e3f62a84503402
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-01-23 21:23:33 -0500

    Update to did-web-resolver v4.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -15,7 +15,6 @@ declare module 'jsonld-document-loader';
 declare module 'did-context';
 declare module 'ed25519-signature-2020-context';
 declare module 'x25519-key-agreement-2020-context';
-declare module '@interop/did-web-resolver';
 declare module 'json-canonicalize';
 declare module 'react-native-keychain';
 declare module '@digitalcredentials/open-badges-context';
