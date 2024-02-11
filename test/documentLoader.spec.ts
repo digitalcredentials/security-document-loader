86f96d7beb7a94e7ac3b0541f1e8ff93cdadb101
Dmitri Zagidulin <dzagidulin@gmail.com>
2024-02-10 19:56:02 -0500

    Switch http handler to use http-client instead of fetch global.

--- test/documentLoader.spec.ts
+++ test/documentLoader.spec.ts
@@ -15,6 +15,15 @@ describe('documentLoader', () => {
     expect(result.document).to.equal(contextObject)
   })
 
+  it('should load a status VC from web', async () => {
+    const documentLoader = securityLoader({fetchRemoteContexts: true}).build();
+
+    const url = 'https://digitalcredentials.github.io/credential-status-playground/JWZM3H8WKU';
+    const result = await documentLoader(url);
+    expect(result.document.issuer).to
+      .equal('did:key:z6MkhVTX9BF3NGYX6cc7jWpbNnR7cAjH8LUffabZP8Qu4ysC');
+  });
+
   it('supports beta OBv3 context', async () => {
     const load = securityLoader().build()
     const { document } = await load('https://purl.imsglobal.org/spec/ob/v3p0/context.json')
